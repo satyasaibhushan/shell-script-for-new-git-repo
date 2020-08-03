bc6ddf91413cc6b4a41fbf28f74cad16dc25782e
Sai Bhushan <40578313+satyasaibhushan@users.noreply.github.com>
2020-08-04 00:04:26 +0530

    added description to be given in the terminal

--- git_repo_creation.sh
+++ git_repo_creation.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 # GitHub API Token
-GH_API_TOKEN='***'
+GH_API_TOKEN='****'
 # GitHub User Name
-GH_USER_NAME='***'
+GH_USER_NAME='****'
 # Variable to store first argument to setup-repo, the repo name. Will be used as GH repo name, too.
 REPO_NAME=$1
 # Store current working directory.
 CURRENT_DIR=$PWD
 # Project directory can be passed as second argument to setup-repo, or will default to current working directory.
 PROJECT_DIR=${2:-$CURRENT_DIR}
+# Specified if folder does not exist and needed to be created
 IS_NEW=$3
-# GitHub repos Create API call
+
+if [ -z $1 ]
+ then 
+   echo " " ; echo ""
+   echo " usage : new-git-repo [--repository-name] [--directory-location] [--new-folder-creation]"
+   echo ""
+   echo "'repositiory name' is same as the name of the required repository name on github, it gives the same name if new folder is created"
+   echo ""
+   echo "'direactory location' is the abbsolute(~/....) or the relative(./....) path of the folder location in the computer "
+   echo ""
+   echo "the third field is specified if the folder does not exist and needed to be created in the given location "
+   echo "any given input is considered is yes for the folder creation"
+   echo " " ; echo ""
+else  
+    curl -H "Authorization: token $GH_API_TOKEN" https://api.github.com/user/repos -d '{"name": "'"${REPO_NAME}"'"}'
+    cd $PROJECT_DIR
+
     if [ ! -z $3 ];
      then 
         mkdir $REPO_NAME
@@ -20,7 +37,6 @@ IS_NEW=$3
      fi    
 
         git init 
-        # Initialize Git in project directory, and add the GH repo remote.
         if [  -z $2 ] || [ ! -z $3  ] ;
         then 
             touch README.md
@@ -38,3 +54,4 @@ IS_NEW=$3
         fi
         git remote add origin https://github.com/${GH_USER_NAME}/${REPO_NAME}.git
         git push -u origin master  
+fi
