8d1e41311fcb0e95b45f07652ebf64e4e9962868
Sai Bhushan <40578313+satyasaibhushan@users.noreply.github.com>
2020-08-03 06:31:35 +0530

    Added option for a new repo with a new directory

--- git_repo_creation.sh
+++ git_repo_creation.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
 # GitHub API Token
-GH_API_TOKEN='*****'
+GH_API_TOKEN='7aae01d01c6e4d1d3f642b1b2fb7866056f863e6'
 # GitHub User Name
-GH_USER_NAME='****'
+GH_USER_NAME='satyasaibhushan'
 # Variable to store first argument to setup-repo, the repo name. Will be used as GH repo name, too.
 REPO_NAME=$1
 # Store current working directory.
 CURRENT_DIR=$PWD
 # Project directory can be passed as second argument to setup-repo, or will default to current working directory.
 PROJECT_DIR=${2:-$CURRENT_DIR}
+IS_NEW=$3
 # GitHub repos Create API call
-curl -H "Authorization: token $GH_API_TOKEN" https://api.github.com/user/repos -d '{"name": "'"${REPO_NAME}"'"}'
-cd $PROJECT_DIR
+    if [ ! -z $3 ];
+     then 
+        mkdir $REPO_NAME
+         cd $REPO_NAME
+        pwd 
+     fi    
 
-git init 
-# Initialize Git in project directory, and add the GH repo remote.
-if [ -z $2 ];
-then 
-    touch README.md
-    git add README.md
-    git commit -m "initial commit with empty readme"
-    
-else        
-    read -p "Enter the commit message: " Message
-    read -p "Do you need a readMe(Y/N): " IsReadmeNeeded
-    if ["$IsReadmeNeeded "  = "Y"]
-    then 
-        touch README.md
-    fi    
-    git add .
-    git commit -m "$Message"
-fi
-git remote add origin https://github.com/${GH_USER_NAME}/${REPO_NAME}.git
-git push -u origin master  
+        git init 
+        # Initialize Git in project directory, and add the GH repo remote.
+        if [  -z $2 ] || [ ! -z $3  ] ;
+        then 
+            touch README.md
+            git add README.md
+            git commit -m "initial commit with empty readme"
+        else        
+            read -p "Enter the commit message: " Message
+            read -p "Do you need a readMe(Y/N): " IsReadmeNeeded
+            if [ "$IsReadmeNeeded"  = "Y" ]
+            then 
+                touch README.md
+            fi    
+            git add .
+            git commit -m "$Message"    
+        fi
+        git remote add origin https://github.com/${GH_USER_NAME}/${REPO_NAME}.git
+        git push -u origin master  
